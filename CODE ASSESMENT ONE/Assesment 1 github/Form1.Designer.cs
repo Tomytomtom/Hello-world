9ea78c42468ca2f93dbeb8842bbd2167ae0e421f
Tomytomtom <Tomytomtom678@gmail.com>
2017-04-13 16:35:13 +0800

    Add files via upload

---
+++ CODE ASSESMENT ONE/Assesment 1 github/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Assesment_1_github
+{
+    partial class CONVERTER
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MEtoFE_Input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CEtoIN_Input = new System.Windows.Forms.TextBox();
+            this.CMtoIN_Button = new System.Windows.Forms.Button();
+            this.MEtoFE_Button = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MEtoFE_Label = new System.Windows.Forms.Label();
+            this.CEtoIN_Lable = new System.Windows.Forms.Label();
+            this.CEtoFE_Label = new System.Windows.Forms.Label();
+            this.CEtoFE_Input = new System.Windows.Forms.TextBox();
+            this.CELtoFA_Input = new System.Windows.Forms.TextBox();
+            this.KItoMI_Input = new System.Windows.Forms.TextBox();
+            this.CltoFahbutton = new System.Windows.Forms.Button();
+            this.KItoMI_Button = new System.Windows.Forms.Button();
+            this.CNtoFE_Button = new System.Windows.Forms.Button();
+            this.CELtoFA_Label = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.CEtoIN_Ouput = new System.Windows.Forms.TextBox();
+            this.CEtoFE_Output = new System.Windows.Forms.TextBox();
+            this.CELtoFA_Output = new System.Windows.Forms.TextBox();
+            this.MEtoFE_Output = new System.Windows.Forms.TextBox();
+            this.kiomil_lable = new System.Windows.Forms.Label();
+            this.KItoMI_Output = new System.Windows.Forms.TextBox();
+            this.EXIT_PROGRAM = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MEtoFE_Input
+            // 
+            this.MEtoFE_Input.Location = new System.Drawing.Point(52, 266);
+            this.MEtoFE_Input.Name = "MEtoFE_Input";
+            this.MEtoFE_Input.Size = new System.Drawing.Size(100, 20);
+            this.MEtoFE_Input.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // CEtoIN_Input
+            // 
+            this.CEtoIN_Input.Location = new System.Drawing.Point(52, 94);
+            this.CEtoIN_Input.Name = "CEtoIN_Input";
+            this.CEtoIN_Input.Size = new System.Drawing.Size(100, 20);
+            this.CEtoIN_Input.TabIndex = 4;
+            // 
+            // CMtoIN_Button
+            // 
+            this.CMtoIN_Button.CausesValidation = false;
+            this.CMtoIN_Button.Location = new System.Drawing.Point(61, 123);
+            this.CMtoIN_Button.Name = "CMtoIN_Button";
+            this.CMtoIN_Button.Size = new System.Drawing.Size(84, 23);
+            this.CMtoIN_Button.TabIndex = 8;
+            this.CMtoIN_Button.Text = "Convert";
+            this.CMtoIN_Button.UseVisualStyleBackColor = true;
+            this.CMtoIN_Button.Click += new System.EventHandler(this.CMtoIN_Button_Click);
+            // 
+            // MEtoFE_Button
+            // 
+            this.MEtoFE_Button.Location = new System.Drawing.Point(61, 311);
+            this.MEtoFE_Button.Name = "MEtoFE_Button";
+            this.MEtoFE_Button.Size = new System.Drawing.Size(75, 23);
+            this.MEtoFE_Button.TabIndex = 9;
+            this.MEtoFE_Button.Text = "Convert ";
+            this.MEtoFE_Button.UseVisualStyleBackColor = true;
+            this.MEtoFE_Button.Click += new System.EventHandler(this.MEtoFE_Button_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 10;
+            // 
+            // MEtoFE_Label
+            // 
+            this.MEtoFE_Label.AutoSize = true;
+            this.MEtoFE_Label.Location = new System.Drawing.Point(61, 234);
+            this.MEtoFE_Label.Name = "MEtoFE_Label";
+            this.MEtoFE_Label.Size = new System.Drawing.Size(75, 13);
+            this.MEtoFE_Label.TabIndex = 11;
+            this.MEtoFE_Label.Text = "Metres to Feet";
+            // 
+            // CEtoIN_Lable
+            // 
+            this.CEtoIN_Lable.AutoSize = true;
+            this.CEtoIN_Lable.Location = new System.Drawing.Point(49, 64);
+            this.CEtoIN_Lable.Name = "CEtoIN_Lable";
+            this.CEtoIN_Lable.Size = new System.Drawing.Size(112, 13);
+            this.CEtoIN_Lable.TabIndex = 12;
+            this.CEtoIN_Lable.Text = "Centimetres to Inches ";
+            // 
+            // CEtoFE_Label
+            // 
+            this.CEtoFE_Label.AutoSize = true;
+            this.CEtoFE_Label.Location = new System.Drawing.Point(274, 64);
+            this.CEtoFE_Label.Name = "CEtoFE_Label";
+            this.CEtoFE_Label.Size = new System.Drawing.Size(98, 13);
+            this.CEtoFE_Label.TabIndex = 13;
+            this.CEtoFE_Label.Text = "Centimetres to Feet";
+            // 
+            // CEtoFE_Input
+            // 
+            this.CEtoFE_Input.Location = new System.Drawing.Point(272, 94);
+            this.CEtoFE_Input.Name = "CEtoFE_Input";
+            this.CEtoFE_Input.Size = new System.Drawing.Size(100, 20);
+            this.CEtoFE_Input.TabIndex = 14;
+            // 
+            // CELtoFA_Input
+            // 
+            this.CELtoFA_Input.Location = new System.Drawing.Point(272, 266);
+            this.CELtoFA_Input.Name = "CELtoFA_Input";
+            this.CELtoFA_Input.Size = new System.Drawing.Size(100, 20);
+            this.CELtoFA_Input.TabIndex = 15;
+            // 
+            // KItoMI_Input
+            // 
+            this.KItoMI_Input.Location = new System.Drawing.Point(162, 435);
+            this.KItoMI_Input.Name = "KItoMI_Input";
+            this.KItoMI_Input.Size = new System.Drawing.Size(100, 20);
+            this.KItoMI_Input.TabIndex = 16;
+            // 
+            // CltoFahbutton
+            // 
+            this.CltoFahbutton.Location = new System.Drawing.Point(284, 311);
+            this.CltoFahbutton.Name = "CltoFahbutton";
+            this.CltoFahbutton.Size = new System.Drawing.Size(75, 23);
+            this.CltoFahbutton.TabIndex = 17;
+            this.CltoFahbutton.Text = "Convert";
+            this.CltoFahbutton.UseVisualStyleBackColor = true;
+            this.CltoFahbutton.Click += new System.EventHandler(this.CltoFahbutton_Click);
+            // 
+            // KItoMI_Button
+            // 
+            this.KItoMI_Button.Location = new System.Drawing.Point(177, 471);
+            this.KItoMI_Button.Name = "KItoMI_Button";
+            this.KItoMI_Button.Size = new System.Drawing.Size(75, 23);
+            this.KItoMI_Button.TabIndex = 18;
+            this.KItoMI_Button.Text = "Convert";
+            this.KItoMI_Button.UseVisualStyleBackColor = true;
+            this.KItoMI_Button.Click += new System.EventHandler(this.KItoMI_Button_Click);
+            // 
+            // CNtoFE_Button
+            // 
+            this.CNtoFE_Button.Location = new System.Drawing.Point(284, 123);
+            this.CNtoFE_Button.Name = "CNtoFE_Button";
+            this.CNtoFE_Button.Size = new System.Drawing.Size(75, 23);
+            this.CNtoFE_Button.TabIndex = 19;
+            this.CNtoFE_Button.Text = "Convert ";
+            this.CNtoFE_Button.UseVisualStyleBackColor = true;
+            this.CNtoFE_Button.Click += new System.EventHandler(this.CNtoFE_Button_Click);
+            // 
+            // CELtoFA_Label
+            // 
+            this.CELtoFA_Label.AutoSize = true;
+            this.CELtoFA_Label.Location = new System.Drawing.Point(269, 234);
+            this.CELtoFA_Label.Name = "CELtoFA_Label";
+            this.CELtoFA_Label.Size = new System.Drawing.Size(108, 13);
+            this.CELtoFA_Label.TabIndex = 20;
+            this.CELtoFA_Label.Text = "Celsius to Fahrenheit ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(130, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(193, 31);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "CONVERTER ";
+            // 
+            // CEtoIN_Ouput
+            // 
+            this.CEtoIN_Ouput.Location = new System.Drawing.Point(52, 165);
+            this.CEtoIN_Ouput.Name = "CEtoIN_Ouput";
+            this.CEtoIN_Ouput.Size = new System.Drawing.Size(100, 20);
+            this.CEtoIN_Ouput.TabIndex = 22;
+            // 
+            // CEtoFE_Output
+            // 
+            this.CEtoFE_Output.Location = new System.Drawing.Point(272, 165);
+            this.CEtoFE_Output.Name = "CEtoFE_Output";
+            this.CEtoFE_Output.Size = new System.Drawing.Size(100, 20);
+            this.CEtoFE_Output.TabIndex = 23;
+            // 
+            // CELtoFA_Output
+            // 
+            this.CELtoFA_Output.Location = new System.Drawing.Point(272, 349);
+            this.CELtoFA_Output.Name = "CELtoFA_Output";
+            this.CELtoFA_Output.Size = new System.Drawing.Size(100, 20);
+            this.CELtoFA_Output.TabIndex = 24;
+            // 
+            // MEtoFE_Output
+            // 
+            this.MEtoFE_Output.Location = new System.Drawing.Point(52, 349);
+            this.MEtoFE_Output.Name = "MEtoFE_Output";
+            this.MEtoFE_Output.Size = new System.Drawing.Size(100, 20);
+            this.MEtoFE_Output.TabIndex = 25;
+            // 
+            // kiomil_lable
+            // 
+            this.kiomil_lable.AutoSize = true;
+            this.kiomil_lable.Location = new System.Drawing.Point(143, 400);
+            this.kiomil_lable.Name = "kiomil_lable";
+            this.kiomil_lable.Size = new System.Drawing.Size(137, 13);
+            this.kiomil_lable.TabIndex = 26;
+            this.kiomil_lable.Text = "Convert Kilometres to Miles ";
+            // 
+            // KItoMI_Output
+            // 
+            this.KItoMI_Output.Location = new System.Drawing.Point(162, 500);
+            this.KItoMI_Output.Name = "KItoMI_Output";
+            this.KItoMI_Output.Size = new System.Drawing.Size(100, 20);
+            this.KItoMI_Output.TabIndex = 27;
+            // 
+            // EXIT_PROGRAM
+            // 
+            this.EXIT_PROGRAM.Location = new System.Drawing.Point(109, 559);
+            this.EXIT_PROGRAM.Name = "EXIT_PROGRAM";
+            this.EXIT_PROGRAM.Size = new System.Drawing.Size(203, 23);
+            this.EXIT_PROGRAM.TabIndex = 28;
+            this.EXIT_PROGRAM.Text = "EXIT PROGRAM  ";
+            this.EXIT_PROGRAM.UseVisualStyleBackColor = true;
+            this.EXIT_PROGRAM.Click += new System.EventHandler(this.EXIT_PROGRAM_Click);
+            // 
+            // CONVERTER
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.HotPink;
+            this.ClientSize = new System.Drawing.Size(432, 614);
+            this.Controls.Add(this.EXIT_PROGRAM);
+            this.Controls.Add(this.KItoMI_Output);
+            this.Controls.Add(this.kiomil_lable);
+            this.Controls.Add(this.MEtoFE_Output);
+            this.Controls.Add(this.CELtoFA_Output);
+            this.Controls.Add(this.CEtoFE_Output);
+            this.Controls.Add(this.CEtoIN_Ouput);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.CELtoFA_Label);
+            this.Controls.Add(this.CNtoFE_Button);
+            this.Controls.Add(this.KItoMI_Button);
+            this.Controls.Add(this.CltoFahbutton);
+            this.Controls.Add(this.KItoMI_Input);
+            this.Controls.Add(this.CELtoFA_Input);
+            this.Controls.Add(this.CEtoFE_Input);
+            this.Controls.Add(this.CEtoFE_Label);
+            this.Controls.Add(this.CEtoIN_Lable);
+            this.Controls.Add(this.MEtoFE_Label);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.MEtoFE_Button);
+            this.Controls.Add(this.CMtoIN_Button);
+            this.Controls.Add(this.CEtoIN_Input);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MEtoFE_Input);
+            this.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.Name = "CONVERTER";
+            this.Text = "CONVERTER";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox MEtoFE_Input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox CEtoIN_Input;
+        private System.Windows.Forms.Button CMtoIN_Button;
+        private System.Windows.Forms.Button MEtoFE_Button;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label MEtoFE_Label;
+        private System.Windows.Forms.Label CEtoIN_Lable;
+        private System.Windows.Forms.Label CEtoFE_Label;
+        private System.Windows.Forms.TextBox CEtoFE_Input;
+        private System.Windows.Forms.TextBox CELtoFA_Input;
+        private System.Windows.Forms.TextBox KItoMI_Input;
+        private System.Windows.Forms.Button CltoFahbutton;
+        private System.Windows.Forms.Button KItoMI_Button;
+        private System.Windows.Forms.Button CNtoFE_Button;
+        private System.Windows.Forms.Label CELtoFA_Label;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox CEtoIN_Ouput;
+        private System.Windows.Forms.TextBox CEtoFE_Output;
+        private System.Windows.Forms.TextBox CELtoFA_Output;
+        private System.Windows.Forms.TextBox MEtoFE_Output;
+        private System.Windows.Forms.Label kiomil_lable;
+        private System.Windows.Forms.TextBox KItoMI_Output;
+        private System.Windows.Forms.Button EXIT_PROGRAM;
+    }
+}
+
---
+++ CODE ASSESMENT ONE/Assesment 1 github/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assesment_1_github
+{
+    public partial class CONVERTER : Form
+    {
+        public CONVERTER()
+        {
+            InitializeComponent();
+        }
+        double converter_result;
+        //arry to stor the results for the covertion 
+        double[] results = new double[4];
+     
+        //Centmeter to 
+        private void CMtoIN_Button_Click(object sender, EventArgs e)
+        {
+            const double CMtoIN = 0.3937;
+            
+            if (! double.TryParse(CEtoIN_Input.Text, out converter_result))
+            {   //
+                MessageBox.Show("Please enter a valid number");
+                // gets rid of anything in the input
+                CEtoIN_Input.Clear();
+                CEtoIN_Input.Focus();
+                // gets rid of anything in the output
+                CEtoIN_Ouput.Clear();
+
+            }
+             else
+            {
+                // doing the maths for the conversion
+                converter_result = CMtoIN * double.Parse(CEtoIN_Input.Text);
+                //printing the result ouit for the user
+                CEtoIN_Ouput.Text = converter_result.ToString();
+                // storing the result of the conversion in an arry
+                results[0] = converter_result;
+                
+               
+            }
+
+        }
+        //
+        private void CNtoFE_Button_Click(object sender, EventArgs e)
+        {   // variable used to calculate the conversion
+            double CEtoFE = 0.0328;
+
+            if (!double.TryParse(CEtoFE_Input.Text, out converter_result))
+            {   // desplays Message to the user leting them know they have tryed to brake my app!!
+                MessageBox.Show("Please enter a valid number");
+                // gets rid of anything in the input
+                CEtoFE_Input.Clear();
+                CEtoFE_Input.Focus();
+                // gets rid of anything in the output
+                CEtoFE_Output.Clear();
+
+            }
+            else
+            {    // doing the maths for the conversion
+                converter_result = CEtoFE * double.Parse(CEtoFE_Input.Text);
+                //printing the result ouit for the user
+                CEtoFE_Output.Text = converter_result.ToString();
+                // storing the result of the conversion in an arry
+                results[1] = converter_result;
+            }
+
+        }
+        //
+        private void CltoFahbutton_Click(object sender, EventArgs e)
+        {  // variable used to calculate the conversion
+            double CELtoFA = 1.8 ;
+             
+            if (!double.TryParse(CELtoFA_Input.Text, out converter_result))
+            { // desplays Message to the user leting them know they have tryed to brake my app!!
+                MessageBox.Show("Please enter a valid number");
+                // gets rid of anything in the input
+                CELtoFA_Input.Clear();
+                CELtoFA_Input.Focus();
+                // gets rid of anything in the output
+                CELtoFA_Output.Clear();
+
+            }
+            else
+            {
+                // doing the maths for the conversion
+                converter_result = CELtoFA * double.Parse(CELtoFA_Input.Text) + 32;
+                //printing the result ouit for the user
+                CELtoFA_Output.Text = converter_result.ToString();
+                // storing the result of the conversion in an arry
+                results[2] = converter_result;
+            }
+        }
+        //
+        private void MEtoFE_Button_Click(object sender, EventArgs e)
+        {
+            // variable used to calculate the conversion
+            double MEtoFE = 3.2808;
+            if (!double.TryParse(MEtoFE_Input.Text, out converter_result))
+            {  // desplays Message to the user leting them know they have tryed to brake my app!!
+                MessageBox.Show("Please enter a valid number");
+                MEtoFE_Input.Clear();
+                MEtoFE_Input.Focus();
+                // gets rid of anything in the output
+                MEtoFE_Output.Clear();
+
+            }
+            else
+            {
+                // doing the maths for the conversion
+                converter_result = MEtoFE * double.Parse(MEtoFE_Input.Text);
+                //printing the result ouit for the user
+                MEtoFE_Output.Text = converter_result.ToString();
+                // storing the result of the conversion in an arry
+                results[3] = converter_result;
+            }
+
+        }
+        //
+        private void KItoMI_Button_Click(object sender, EventArgs e)
+        {
+            // variable used to calculate the conversion
+            double KItoMI = 0.6213 ;
+
+            if (!double.TryParse(KItoMI_Input.Text, out converter_result))
+            { // desplays Message to the user leting them know they have tryed to brake my app!!
+                MessageBox.Show("Please enter a valid number");
+                // gets rid of anything in the input
+                KItoMI_Input.Clear();
+                KItoMI_Input.Focus();
+                // gets rid of anything in the output
+                KItoMI_Output.Clear();
+
+            }
+            else
+            {
+                // doing the maths for the conversion
+                converter_result = KItoMI * double.Parse(KItoMI_Input.Text);
+                //printing the result ouit for the user
+                KItoMI_Output.Text = converter_result.ToString();
+                // storing the result of the conversion in an arry
+                results[4] = converter_result;
+            }
+        }
+        //  exits app when the exit button is pressed
+        private void EXIT_PROGRAM_Click(object sender, EventArgs e)
+        {
+            // exits aplication 
+            Application.Exit();
+        }
+    }
+}
